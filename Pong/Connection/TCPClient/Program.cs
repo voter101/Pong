8212b6d9650c23a4a687d15c433fbcb1186b2386
Voter101 <voter101@gmail.com>
2013-09-21 20:50:12 +0200

    Connection classes snapshot. Requires refactoring.

---
+++ Pong/Connection/TCPClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ip = "127.0.0.1";
+            Client tcpClient = Client.Instance;
+            tcpClient.Initialize(ip, 1337);
+            Console.WriteLine("Kuniec");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pong/Connection/TCPServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = Server.Instance;
+            server.Initialize(1337);
+            Console.ReadKey();
+        }
+    }
+}
